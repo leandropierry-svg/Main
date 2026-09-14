5f3fd53f6354aca49eb9ac3a2bc68af9b48b1c8d
Pierre Mitson <leandropierry@gmail.com>
2026-09-13 21:12:16 -0300

    teste

--- teste/teste 02.py
+++
@@ -1,4 +0,0 @@
-dia = input ( 'digite o dia de nascimento? ' )
-mes = input ( 'digite o mes de nascimento? ' )
-ano = input ( 'digite o ano de nascimento? ' )
-print (f' voce nasceu no dia {dia} de {mes} de {ano} correto? ')
